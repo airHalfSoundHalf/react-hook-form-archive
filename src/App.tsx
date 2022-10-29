b72768212c41a25521ed3f1a1428cdaff5ea2716
bpthess <bpthess@naver.com>
2022-10-30 02:13:33 +0900

    에러 반환 처리 고민중

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
 import "./App.css";
+import { useForm } from "react-hook-form";
 
 function App() {
-  return <></>;
+  const {
+    register,
+    watch,
+    formState: { errors },
+  } = useForm();
+  console.log(watch("email"));
+
+  return (
+    <form
+    // onSubmit={handleSubmit(onSubmit)}
+    >
+      <label>Email</label>
+      <input type="email" {...register("email")} />
+      {errors.email && <p>이메일을 올바르게 입력하세요.</p>}
+      <label>Name</label>
+      <input name="name" />
+      <label>Password</label>
+      <input name="password" type="password" />
+      <label>Password Confirm</label>
+      <input name="password_confirm" type="password" />
+      <input type="submit" />
+    </form>
+  );
 }
 
 export default App;
