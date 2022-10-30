34751b1b4bcd6a11d13a889b8b60000f263358ec
bpthess <bpthess@naver.com>
2022-10-31 03:32:25 +0900

    폼 데이터 진행중

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,55 @@
 import "./App.css";
 import { useForm } from "react-hook-form";
+import { useRef } from "react";
 
 function App() {
   const {
     register,
     watch,
     formState: { errors },
+    handleSubmit,
   } = useForm();
   console.log(watch("email"));
 
+  const password = useRef();
+
+  const onSubmit = (data: any) => {
+    console.log("data", data);
+  };
+
   return (
-    <form
-    // onSubmit={handleSubmit(onSubmit)}
-    >
+    <form onSubmit={handleSubmit(onSubmit)}>
       <label>Email</label>
-      <input type="email" {...register("email")} />
+      <input
+        type="email"
+        {...register("email")}
+        required={true}
+        // pattern={"/^S+@S+$/i"}
+      />
       {errors.email && <p>이메일을 올바르게 입력하세요.</p>}
       <label>Name</label>
       <input name="name" />
+      {errors.name && <p>이메일을 올바르게 입력하세요.</p>}
       <label>Password</label>
-      <input name="password" type="password" />
+      <input
+        // name="password"
+        type="password"
+        {...register("password")}
+        required={true}
+        minLength={6}
+      />
+      {errors.password && <p>패스워드를 올바르게 입력하세요.</p>}
       <label>Password Confirm</label>
-      <input name="password_confirm" type="password" />
+      <input type="password" {...register("password")} required={true} />
+
+      {errors.password_confirm &&
+        errors.password_confirm.type === "required" && (
+          <p>비밀번호를 입력하세요.</p>
+        )}
+      {errors.password_confirm &&
+        errors.password_confirm.type === "validate" && (
+          <p>비밀번호와 중복 비밀번호가 일치하지 않습니다.</p>
+        )}
       <input type="submit" />
     </form>
   );
